b0c2f32dcde7fd7b7d443e87a0e45b102f7cec11
Bartek <bartekzadlo2004@gmail.com>
2025-01-28 03:47:39 +0100

    dodanie funkcji ogolnej dzialanie klienta

--- klient.c
+++ klient.c
@@ -106,3 +106,54 @@ void otrzymaj_reszte(Klient *klient)
 {
     printf("Tu informacja od fryzjera o reszcie.\n");
 }
+
+void dzialanie_klienta(Salon *salon, Klient *klient, int Tp, int Tk)
+{
+    // Klient najpierw zarabia pieniądze
+    zarabiaj_pieniadze(klient);
+
+    // Losowanie godziny przyjścia
+    int czas_przyjscia = rand() % (Tk - Tp + 1) + Tp;
+    printf("Klient pojawia się o godzinie %d:00.\n", czas_przyjscia);
+
+    while (1)
+    {
+        // Klient stara się wejść do salonu
+        if (sem_trywait(&salon->poczekalnia) == 0)
+        {
+            // Klient wszedł do poczekalni
+            printf("Klient wchodzi do poczekalni.\n");
+
+            // Zablokowanie dostępu do poczekalni
+            pthread_mutex_lock(&salon->mutex_poczekalnia);
+            salon->klienci_w_poczekalni++;
+            pthread_mutex_unlock(&salon->mutex_poczekalnia);
+
+            // Czekanie na fotel
+            printf("Czekanie na uzupełnienie fryzjera.\n");
+            zajmij_fotel(&salon->fotel);
+
+            // Tutaj fryzjer obsługuje klienta
+            printf("Fryzjer obsługuje klienta.\n");
+
+            // Zmniejszenie liczby klientów w poczekalni
+            pthread_mutex_lock(&salon->mutex_poczekalnia);
+            salon->klienci_w_poczekalni--;
+            pthread_mutex_unlock(&salon->mutex_poczekalnia);
+
+            // Zwolnienie miejsca w poczekalni
+            sem_post(&salon->poczekalnia);
+            break;
+        }
+        else
+        {
+            // Brak wolnych miejsc w poczekalni, klient wraca do zarabiania
+            printf("Brak wolnych miejsc w poczekalni. Klient wraca do zarabiania.\n");
+            zarabiaj_pieniadze(klient);
+
+            // Losowanie godziny na kolejny dzień
+            czas_przyjscia = rand() % (Tk - Tp + 1) + Tp;
+            printf("Nowa godzina przyjścia: %d:00\n", czas_przyjscia);
+        }
+    }
+}
