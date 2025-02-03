1850513ec4e924de65917bf17585de94a38d699b
Bartek <bartekzadlo2004@gmail.com>
2025-02-03 21:20:18 +0100

    zmiany w strukturach, dodanie kolejki - poczekalni

--- common.h
+++ common.h
@@ -10,62 +10,32 @@
 #define P 10          // liczba klientów
 #define MAX_WAITING K // rozmiar kolejki poczekalni
 
+/* Struktura opisująca klienta */
 typedef struct
 {
-    int id;                           // Id Klienta
-    int portfel_10;                   // Zawartość portfela klienta
-    int portfel_20;                   // Zawartość portfela klienta
-    int portfel_50;                   // Zawartość portfela klienta
-    pthread_cond_t czekaj_na_zaplate; // Warunek dla klienta od fryzjera że ten czeka na zapłate
-    pthread_mutex_t mutex_klient;     // Mutex dostępu do klienta
-    pthread_t watek;                  // Wątek klienta
+    int id;         // Id Klienta
+    int portfel_10; // Zawartość portfela klienta
+    int portfel_20; // Zawartość portfela klienta
+    int portfel_50; // Zawartość portfela klienta
+    sem_t served;   // semafor, na którym klient czeka na zakończenie obsługi
 } Klient;
 
-// Struktura Fotel
-typedef struct
-{
-    sem_t wolne_fotele;          // Semafor dla wolnych foteli
-    pthread_mutex_t mutex_fotel; // Mutex do synchronizacji dostępu do fotela
-} Fotel;
+/* Kolejka poczekalni – implementowana jako tablica cykliczna */
+Klient *poczekalnia[MAX_WAITING];
+int poczekalniaFront = 0; // indeks pierwszego oczekującego klienta
+int poczekalniaCount = 0; // liczba klientów w poczekalni
+pthread_mutex_t poczekalniaMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t poczekalniaNotEmpty = PTHREAD_COND_INITIALIZER;
 
 // Struktura Kasa
 typedef struct
 {
-    int banknot_10;               // ilosc banknotow w kasie
-    int banknot_20;               // ilosc banknotow w kasie
-    int banknot_50;               // ilosc banknotow w kasie
-    int wydane_10;                // uzywane przy zapamiętaniu ile wydajemy
-    int wydane_20;                // uzywane przy zapamiętaniu ile wydajemy
-    int wydane_50;                // uzywane przy zapamiętaniu ile wydajemy
-    pthread_cond_t uzupelnienie;  // Sygnał dla fryzjera, że kasa uzupełniona
-    pthread_cond_t zaplata;       // Sygnał dla fryzjera, że klient zapłacił
-    pthread_cond_t wydano_reszte; // Warunek dla klienta, że fryzjer wydaje resztę
-    pthread_mutex_t mutex_kasa;   // Mutex do synchronizacji dostępu do kasy
+    int banknot_10;              // ilosc banknotow w kasie
+    int banknot_20;              // ilosc banknotow w kasie
+    int banknot_50;              // ilosc banknotow w kasie
+    pthread_cond_t uzupelnienie; // Sygnał dla fryzjera, że kasa uzupełniona
+    pthread_mutex_t mutex_kasa;  // Mutex do synchronizacji dostępu do kasy
 } Kasa;
-
-// Struktura Salon
-typedef struct
-{
-    sem_t poczekalnia;                 // Semafor dla poczekalni
-    pthread_mutex_t mutex_poczekalnia; // Mutex do synchronizacji dostępu do poczekalni
-    int klienci_w_poczekalni;          // Liczba klientów w poczekalni
-    int max_klientow;
-    Fotel fotel;         // Fotel z semaforem i mutexem
-    Kasa kasa;           // Kasa z banknotami i mutexem
-    int zaplacone_10;    // uzywane przy placeniu za usluge
-    int zaplacone_20;    // uzywane przy placeniu za usluge
-    int zaplacone_50;    // uzywane przy placeniu za usluge
-    int zaplacona_kwota; // uzywane przy placeniu za usluge
-    int reszta;
-} Salon;
-
-typedef struct
-{
-    int id;          // ID fryzjera
-    Salon *salon;    // Wskaźnik do salonu
-    Klient *klient;  // Wskaźnik na aktualnego klienta
-    pthread_t watek; // Wątek fryzjera
-} Fryzjer;
 
 // Funkcje operujące na salonie
 void inicjalizuj_salon(Salon *salon, int max_klientow, int liczba_foteli);
