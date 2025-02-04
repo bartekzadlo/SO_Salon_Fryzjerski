1bfc0ba8c536630222d1f0b272503a38df52bb20
Bartek <bartekzadlo2004@gmail.com>
2025-02-04 01:44:06 +0100

    dodanie komunikatow do kierownika

--- kierownik.c
+++ kierownik.c
@@ -10,10 +10,12 @@
 void *manager_thread(void *arg)
 {
     (void)arg; // nieużywany parametr
+    char log_buffer[MSG_SIZE];
     /* Po 30 sekundach wysyłamy sygnał 1 do fryzjera 0 */
     sleep(30);
     barber_stop[0] = 1;
-    printf("Kierownik: wysyłam sygnał 1 do fryzjera 0 (skończenie pracy przed zamknięciem salonu).\n");
+    snprintf(log_buffer, MSG_SIZE, "Kierownik: wysyłam sygnał 1 do fryzjera 0 (skończenie pracy przed zamknięciem salonu).");
+    send_message(log_buffer);
 
     /* Po kolejnych 30 sekundach wysyłamy sygnał 2 – wszyscy klienci natychmiast opuszczają salon */
     sleep(30);
@@ -21,12 +23,12 @@ void *manager_thread(void *arg)
     pthread_mutex_lock(&poczekalniaMutex);
     pthread_cond_broadcast(&poczekalniaNotEmpty);
     pthread_mutex_unlock(&poczekalniaMutex);
-
     pthread_mutex_lock(&kasa.mutex_kasa);
     pthread_cond_broadcast(&kasa.uzupelnienie);
     pthread_mutex_unlock(&kasa.mutex_kasa);
 
-    printf("Kierownik: wysyłam sygnał 2 (wszyscy klienci opuszczają salon) oraz zamykam salon.\n");
+    snprintf(log_buffer, MSG_SIZE, "Kierownik: wysyłam sygnał 2 (wszyscy klienci opuszczają salon) oraz zamykam salon.");
+    send_message(log_buffer);
     salon_open = 0;
     return NULL;
 }
