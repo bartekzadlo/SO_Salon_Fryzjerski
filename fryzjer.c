b5e4f453fa3edef8814f1517c54de3a7715aa954
Bartek <bartekzadlo2004@gmail.com>
2025-02-04 01:40:03 +0100

    dodanie komunikatow do watku fryzjera

--- fryzjer.c
+++ fryzjer.c
@@ -12,12 +12,15 @@ void *barber_thread(void *arg)
     int id = *((int *)arg);
     free(arg);
 
+    char log_buffer[MSG_SIZE];
+
     while (1)
     {
         /* Sprawdzenie, czy dla tego fryzjera wysłano sygnał 1 */
         if (barber_stop[id])
         {
             printf("Fryzjer %d: otrzymałem sygnał 1, kończę pracę.\n", id);
+            send_message(log_buffer);
             break;
         }
 
@@ -38,32 +41,34 @@ void *barber_thread(void *arg)
         poczekalniaCount--;
         pthread_mutex_unlock(&poczekalniaMutex);
 
+        snprintf(log_buffer, MSG_SIZE, "Fryzjer %d: rozpoczynam obsługę klienta %d (płatność: %d zł).", id, klient->id, klient->payment);
+        send_message(log_buffer);
         /* Rezerwacja fotela (czekamy, aż fotel będzie wolny) */
         sem_wait(&fotele_semafor);
-        printf("Fryzjer %d: rozpoczynam obsługę klienta %d (płatność: %d zł).\n",
-               id, klient->id, klient->payment);
 
         /* Pobieranie opłaty – pieniądze trafiają do wspólnej kasy */
         pthread_mutex_lock(&kasa.mutex_kasa);
         if (klient->payment == 20)
         {
             kasa.banknot_20++;
-            printf("Fryzjer %d: otrzymałem 20 zł. Kasa: 10zł=%d, 20zł=%d, 50zł=%d.\n",
-                   id, kasa.banknot_10, kasa.banknot_20, kasa.banknot_50);
+            snprintf(log_buffer, MSG_SIZE, "Fryzjer %d: otrzymałem 20 zł. Kasa: 10zł=%d, 20zł=%d, 50zł=%d.",
+                     id, kasa.banknot_10, kasa.banknot_20, kasa.banknot_50);
+            send_message(log_buffer);
         }
         else if (klient->payment == 50)
         {
             kasa.banknot_50++;
-            printf("Fryzjer %d: otrzymałem 50 zł. Kasa: 10zł=%d, 20zł=%d, 50zł=%d.\n",
-                   id, kasa.banknot_10, kasa.banknot_20, kasa.banknot_50);
+            snprintf(log_buffer, MSG_SIZE, "Fryzjer %d: otrzymałem 50 zł. Kasa: 10zł=%d, 20zł=%d, 50zł=%d.",
+                     id, kasa.banknot_10, kasa.banknot_20, kasa.banknot_50);
+            send_message(log_buffer);
         }
         pthread_mutex_unlock(&kasa.mutex_kasa);
 
         /* Realizacja usługi – symulacja czasu strzyżenia (losowo 1–3 s) */
         int service_time = rand() % 3 + 1;
         sleep(service_time);
-        printf("Fryzjer %d: zakończyłem strzyżenie klienta %d (czas usługi: %d s).\n",
-               id, klient->id, service_time);
+        snprintf(log_buffer, MSG_SIZE, "Fryzjer %d: zakończyłem strzyżenie klienta %d (czas usługi: %d s).", id, klient->id, service_time);
+        send_message(log_buffer);
 
         /* Zwalniamy fotel */
         sem_post(&fotele_semafor);
@@ -79,21 +84,23 @@ void *barber_thread(void *arg)
             if (close_all_clients)
             {
                 pthread_mutex_unlock(&kasa.mutex_kasa);
-                printf("Fryzjer %d: salon zamknięty, nie wydaję reszty klientowi %d.\n",
-                       id, klient->id);
+                snprintf(log_buffer, MSG_SIZE, "Fryzjer %d: salon zamknięty, nie wydaję reszty klientowi %d.", id, klient->id);
+                send_message(log_buffer);
                 sem_post(&klient->served);
                 continue;
             }
             kasa.banknot_10--;
             kasa.banknot_20--;
-            printf("Fryzjer %d: wydaję resztę 30 zł (10+20) klientowi %d. Kasa: 10zł=%d, 20zł=%d, 50zł=%d.\n",
-                   id, klient->id, kasa.banknot_10, kasa.banknot_20, kasa.banknot_50);
+            snprintf(log_buffer, MSG_SIZE, "Fryzjer %d: wydaję resztę 30 zł (10+20) klientowi %d. Kasa: 10zł=%d, 20zł=%d, 50zł=%d.",
+                     id, klient->id, kasa.banknot_10, kasa.banknot_20, kasa.banknot_50);
+            send_message(log_buffer);
             pthread_mutex_unlock(&kasa.mutex_kasa);
         }
 
         /* Powiadomienie klienta o zakończeniu obsługi */
         sem_post(&klient->served);
     }
-    printf("Fryzjer %d: wychodzę z pracy.\n", id);
+    snprintf(log_buffer, MSG_SIZE, "Fryzjer %d: wychodzę z pracy.", id);
+    send_message(log_buffer);
     return NULL;
 }
