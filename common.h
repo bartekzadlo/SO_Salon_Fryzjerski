219b541d25b2900dc20e46b955098012f262cfae
Bartek <bartekzadlo2004@gmail.com>
2025-02-04 03:14:04 +0100

    komentarze w pliku naglowkowym

--- common.h
+++ common.h
@@ -4,18 +4,24 @@
 #include <semaphore.h>
 #include <pthread.h>
 
-/* Parametry symulacji */
-#define F 3           // liczba fryzjerów (F > 1)
-#define N 2           // liczba foteli (N < F)
-#define K 5           // maksymalna liczba klientów w poczekalni
-#define P 10          // liczba klientów
-#define MAX_WAITING K // rozmiar kolejki poczekalni
+/* Parametry symulacji            */
+#define F 3           // Liczba fryzjerów (F > 1) – ilość wątków reprezentujących fryzjerów
+#define N 2           // Liczba foteli (N < F) – ograniczenie liczby jednocześnie obsługiwanych klientów
+#define K 5           // Maksymalna liczba klientów w poczekalni
+#define P 10          // Liczba klientów – ilość wątków reprezentujących klientów
+#define MAX_WAITING K // Rozmiar kolejki poczekalni – wykorzystujemy K jako maksymalną liczbę oczekujących klientów
 
 /* Definicja struktury wiadomości */
-#define MSG_SIZE 128
-#define MSG_TYPE_EVENT 1
-#define MSG_TYPE_EXIT 999
+#define MSG_SIZE 128      // Maksymalny rozmiar tekstu w komunikacie
+#define MSG_TYPE_EVENT 1  // Typ komunikatu: zdarzenie (np. log zdarzeń)
+#define MSG_TYPE_EXIT 999 // Typ komunikatu: sygnał zakończenia (używany do zamknięcia loggera)
 
+/*
+ * Struktura Message:
+ * Służy do komunikacji między procesami (np. do logowania zdarzeń) przy użyciu kolejki komunikatów.
+ * mtype – typ komunikatu (np. zdarzenie lub exit)
+ * mtext – tekst komunikatu (do 128 znaków)
+ */
 typedef struct
 {
     long mtype;
@@ -25,60 +31,80 @@ typedef struct
 /* Struktura przechowywana w pamięci współdzielonej */
 typedef struct
 {
-    int total_clients_served; // klienci, którzy zostali obsłużeni
-    int total_clients_left;   // klienci, którzy odeszli (np. gdy poczekalnia była pełna)
-    int total_services_done;  // liczba wykonanych usług (strzyżeń)
+    int total_clients_served; // Liczba klientów, którzy zostali obsłużeni
+    int total_clients_left;   // Liczba klientów, którzy opuścili salon (np. gdy poczekalnia była pełna)
+    int total_services_done;  // Liczba wykonanych usług (np. ilość przeprowadzonych strzyżeń)
 } SalonStats;
-extern SalonStats *sharedStats;
+extern SalonStats *sharedStats; // Wskaźnik do struktury statystyk salonu (przechowywanych w pamięci współdzielonej)
 
-/* Struktura opisująca klienta */
+/* Struktura opisująca klienta     */
 typedef struct
 {
-    int id;       // Id Klienta
-    int payment;  // kwota przekazywana (20 lub 50 zł)
-    sem_t served; // semafor, na którym klient czeka na zakończenie obsługi
+    int id;       // Unikalny identyfikator klienta
+    int payment;  // Kwota przekazywana przez klienta (może wynosić 20 lub 50 zł)
+    sem_t served; // Semafor używany przez klienta do oczekiwania na zakończenie obsługi przez fryzjera
 } Klient;
 
 /* Kolejka poczekalni – implementowana jako tablica cykliczna */
-extern Klient *poczekalnia[MAX_WAITING];
-extern int poczekalniaFront; // indeks pierwszego oczekującego klienta
-extern int poczekalniaCount; // liczba klientów w poczekalni
-extern pthread_mutex_t poczekalniaMutex;
-extern pthread_cond_t poczekalniaNotEmpty;
+extern Klient *poczekalnia[MAX_WAITING];   // Tablica wskaźników na klientów reprezentująca poczekalnię
+extern int poczekalniaFront;               // Indeks pierwszego oczekującego klienta (początek kolejki)
+extern int poczekalniaCount;               // Liczba klientów aktualnie oczekujących w poczekalni
+extern pthread_mutex_t poczekalniaMutex;   // Mutex służący do synchronizacji dostępu do kolejki poczekalni
+extern pthread_cond_t poczekalniaNotEmpty; // Zmienna warunkowa, która sygnalizuje, że poczekalnia nie jest pusta
 
-extern sem_t fotele_semafor;
+/* Semafor foteli                 */
+extern sem_t fotele_semafor; // Semafor kontrolujący liczbę dostępnych foteli (ograniczenie liczby obsługiwanych klientów)
 
-// Struktura Kasa
+/* Struktura Kasa                 */
 typedef struct
 {
-    int banknot_10;              // ilość banknotów w kasie
-    int banknot_20;              // ilość banknotów w kasie
-    int banknot_50;              // ilość banknotów w kasie
-    pthread_cond_t uzupelnienie; // Sygnał dla fryzjera, że kasa uzupełniona
-    pthread_mutex_t mutex_kasa;  // Mutex do synchronizacji dostępu do kasy
+    int banknot_10;              // Liczba banknotów o nominale 10 zł dostępnych w kasie
+    int banknot_20;              // Liczba banknotów o nominale 20 zł dostępnych w kasie
+    int banknot_50;              // Liczba banknotów o nominale 50 zł dostępnych w kasie
+    pthread_cond_t uzupelnienie; // Zmienna warunkowa sygnalizująca fryzjerom, że kasa została uzupełniona
+    pthread_mutex_t mutex_kasa;  // Mutex służący do synchronizacji dostępu do kasy
 } Kasa;
-extern Kasa kasa;
+extern Kasa kasa; // Globalna struktura kasy salonu
 
-/* Flagi sterujące symulacją */
-extern int salon_open;        // salon czynny
-extern int close_all_clients; // sygnał 2: wszyscy klienci natychmiast opuszczają salon
-extern int barber_stop[F];    // dla każdego fryzjera – sygnał 1, aby zakończył pracę
+/* Flagi sterujące symulacją      */
+extern int salon_open;        // Flaga informująca, czy salon jest czynny (1 – otwarty, 0 – zamknięty)
+extern int close_all_clients; // Flaga sygnalizująca, że wszyscy klienci muszą natychmiast opuścić salon (sygnał 2)
+extern int barber_stop[F];    // Tablica flag dla fryzjerów – dla każdego fryzjera sygnał (1), aby zakończył pracę
 
 /* Globalny identyfikator kolejki komunikatów */
-extern int msgqid;
+extern int msgqid; // Identyfikator kolejki komunikatów używanej do logowania zdarzeń
 
-/* Funkcja inicjalizująca kasę */
+/* Prototypy funkcji i procedur  */
+
+/*
+ * Funkcja init_kasa:
+ * Inicjalizuje kasę salonu, ustawiając początkowe wartości banknotów oraz
+ * inicjalizując mutex i zmienną warunkową używaną do synchronizacji operacji na kasie.
+ */
 void init_kasa();
 
-/* Prototypy funkcji wątków */
+/*
+ * Prototypy funkcji wątków:
+ * - barber_thread: Funkcja reprezentująca pracę fryzjera.
+ * - client_thread: Funkcja reprezentująca działanie klienta.
+ * - manager_thread: Funkcja reprezentująca pracę managera (kierownika) salonu.
+ */
 void *barber_thread(void *arg);
 void *client_thread(void *arg);
 void *manager_thread(void *arg);
 
-/* Funkcja pomocnicza wysyłająca komunikat do kolejki */
+/*
+ * Funkcja send_message:
+ * Wysyła komunikat do kolejki komunikatów.
+ * Używana głównie do logowania zdarzeń w symulacji.
+ */
 void send_message(const char *text);
 
-/* Proces loggera – odbiera komunikaty z kolejki i wypisuje je */
+/*
+ * Funkcja logger_process:
+ * Proces loggera, który odbiera komunikaty z kolejki i wypisuje je na standardowe wyjście.
+ * Proces kończy działanie po otrzymaniu komunikatu zakończenia (MSG_TYPE_EXIT).
+ */
 void logger_process();
 
 #endif // COMMON_H
